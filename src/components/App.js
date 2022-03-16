f5bffda21154858b82f8cd1b583775f438afc720
joshua david jennings <jennings.josh@gmail.com>
2022-03-16 00:40:00 -0500

    rendered pizzas and edit button works

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React from "react";
-import Header from "./Header";
-import PizzaForm from "./PizzaForm";
-import PizzaList from "./PizzaList";
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import PizzaForm from './PizzaForm';
+import PizzaList from './PizzaList';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <PizzaForm />
-      <PizzaList />
-    </>
-  );
+	const [pizzas, setPizzas] = useState([]);
+	const [selectedPizza, setSelectedPizza] = useState({});
+
+	useEffect(() => {
+		fetch('http://localhost:3001/pizzas')
+			.then((res) => res.json())
+			.then((data) => setPizzas(data));
+	}, []);
+
+	function selectPizza(pizzaObj) {
+		setSelectedPizza(pizzaObj);
+	}
+
+	return (
+		<>
+			<Header />
+			<PizzaForm selectedPizza={selectedPizza} />
+			<PizzaList pizzas={pizzas} selectPizza={selectPizza} />
+		</>
+	);
 }
 
 export default App;
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,52 +1,62 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function PizzaForm() {
-  return (
-    <form onSubmit={null /*handle that submit*/}>
-      <div className="form-row">
-        <div className="col-5">
-          <input
-            className="form-control"
-            type="text"
-            name="topping"
-            placeholder="Pizza Topping"
-          />
-        </div>
-        <div className="col">
-          <select className="form-control" name="size">
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-          </select>
-        </div>
-        <div className="col">
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="vegetarian"
-              value="Vegetarian"
-            />
-            <label className="form-check-label">Vegetarian</label>
-          </div>
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="vegetarian"
-              value="Not Vegetarian"
-            />
-            <label className="form-check-label">Not Vegetarian</label>
-          </div>
-        </div>
-        <div className="col">
-          <button type="submit" className="btn btn-success">
-            Submit
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+function PizzaForm({ selectedPizza }) {
+	const [topping, setTopping] = useState('');
+	const [size, setSize] = useState('');
+	const [vegetarian, setVegetarian] = useState(false);
+
+	return (
+		<form onSubmit={null /*handle that submit*/}>
+			<div className="form-row">
+				<div className="col-5">
+					<input
+						className="form-control"
+						type="text"
+						name="topping"
+						placeholder="Pizza Topping"
+						onChange={(e) => setTopping(e.target.value)}
+						value={topping}
+					/>
+				</div>
+				<div className="col">
+					<select
+						className="form-control"
+						name="size"
+						onChange={(e) => setSize(e.target.value)}
+					>
+						<option value="Small">Small</option>
+						<option value="Medium">Medium</option>
+						<option value="Large">Large</option>
+					</select>
+				</div>
+				<div className="col">
+					<div className="form-check">
+						<input
+							className="form-check-input"
+							type="radio"
+							name="vegetarian"
+							value="Vegetarian"
+						/>
+						<label className="form-check-label">Vegetarian</label>
+					</div>
+					<div className="form-check">
+						<input
+							className="form-check-input"
+							type="radio"
+							name="vegetarian"
+							value="Not Vegetarian"
+						/>
+						<label className="form-check-label">Not Vegetarian</label>
+					</div>
+				</div>
+				<div className="col">
+					<button type="submit" className="btn btn-success">
+						Submit
+					</button>
+				</div>
+			</div>
+		</form>
+	);
 }
 
 export default PizzaForm;
